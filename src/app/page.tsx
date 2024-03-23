3823f8403d5827db3267303bf697035249b0b7be
Kouler <beeproger@yandex.ru>
2024-03-23 16:04:00 +1000

    I've clone the github repository, made 'npm install', it works, then I've made one change in main page.tsx file and pushed it back to github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <h1>Hello, mthfck!!</h1>
+  return <main>
+    <h1>Hello, mthfck!!</h1>
+    <p>Здесь я склонировал себе проект и запушил его потом обратно</p>
+  </main>
+  
 } 
